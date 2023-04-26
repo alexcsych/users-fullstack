c1434269ce48b0ce74469da36e736387a76d5c92
Alexc <alexsejsi4@gmail.com>
2023-04-26 10:34:16 +0300

    Initial commit

---
+++ client/src/components/UsersList/index.jsx
@@ -0,0 +1,38 @@
+import { connect } from 'react-redux';
+import BeatLoader from 'react-spinners/BeatLoader';
+import styles from './UsersList.module.sass';
+import defImage from './defaultPhoto.jpg';
+import { useEffect, useState } from 'react';
+import { getUsersThunk } from '../../store/slices/usersSlice';
+
+export const UsersList = ({ users, isFetching, error, get }) => {
+  useEffect(() => {
+    get();
+  }, []);
+  return (
+    <>
+      <BeatLoader loading={isFetching} />
+      {error && <div>!!!ERROR!!!</div>}
+      <ul>
+        {users.map(u => (
+          <li key={u.id}>
+            <img
+              className={styles.userImage}
+              src={u.image ? `http://localhost:5000/${u.image}` : defImage}
+              alt={`${u.firstName} ${u.lastName}`}
+            />
+            {JSON.stringify(u)}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+const mapStateToProps = ({ usersData }) => usersData;
+
+const mapDispatchToProps = dispatch => ({
+  get: () => dispatch(getUsersThunk()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(UsersList);
---
+++ client/src/store/slices/usersSlice.js
@@ -0,0 +1,71 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import * as API from './../../api';
+
+const USERS_SLICE_NAME = 'users';
+
+export const getUsersThunk = createAsyncThunk(
+  `${USERS_SLICE_NAME}/get`,
+  async (payload, thunkAPI) => {
+    try {
+      const response = await API.getUsers();
+      return response.data.data;
+    } catch (err) {
+      return thunkAPI.rejectWithValue({ message: err.message });
+    }
+  }
+);
+
+export const createUsersThunk = createAsyncThunk(
+  `${USERS_SLICE_NAME}/post`,
+  async (payload, thunkAPI) => {
+    try {
+      const response = await API.createUser(payload);
+      return response.data.data;
+    } catch (err) {
+      return thunkAPI.rejectWithValue({ message: err.message });
+    }
+  }
+);
+
+const initialState = {
+  users: [],
+  isFetching: false,
+  error: null,
+};
+
+const usersSlice = createSlice({
+  name: USERS_SLICE_NAME,
+  initialState,
+  extraReducers: builder => {
+    //GET
+    builder.addCase(getUsersThunk.pending, (state, action) => {
+      state.isFetching = true;
+      state.error = null;
+    });
+    builder.addCase(getUsersThunk.fulfilled, (state, action) => {
+      state.isFetching = false;
+      state.users = [...action.payload];
+    });
+    builder.addCase(getUsersThunk.rejected, (state, action) => {
+      state.isFetching = false;
+      state.error = action.payload;
+    });
+    //CREATE
+    builder.addCase(createUsersThunk.pending, (state, action) => {
+      state.isFetching = true;
+      state.error = null;
+    });
+    builder.addCase(createUsersThunk.fulfilled, (state, action) => {
+      state.isFetching = false;
+      state.users.push(action.payload);
+    });
+    builder.addCase(createUsersThunk.rejected, (state, action) => {
+      state.isFetching = false;
+      state.error = action.payload;
+    });
+  },
+});
+
+const { reducer } = usersSlice;
+
+export default reducer;
